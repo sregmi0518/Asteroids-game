a3cf70b225b6a34d66799fb8b9c9231f969dd26d
sregmi0518 <miscrit48@gmail.com>
2025-11-22 04:12:28 +0000

    black screen

---
+++ main.py
@@ -0,0 +1,24 @@
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+import pygame
+from logger import log_state
+
+pygame.init()
+
+def main():
+    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
